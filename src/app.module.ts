c003bcaba27f622b5494887d017bd63b20ad799a
wendel-notebook-server <projetoempresa2025@hotmail.com>
2026-01-12 18:03:12 -0300

    Refactoring env config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import { join } from 'node:path';
 
 @Module({
-  providers: [EnvConfigService]
+  providers: [EnvConfigService],
 })
-export class EnvConfigModule {}
+export class EnvConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return {
+      module: EnvConfigModule,
+      imports: [
+        ConfigModule.forRoot({
+          ...options,
+          isGlobal: true,
+          envFilePath: [
+            join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
+          ],
+        }),
+      ],
+    }
+  }
+}
